ae43b4c2fe785ac36354560b99be916d8865c1e0
Ben Morris-Rains <Benmorrisrains@gmail.com>
2018-04-14 15:42:44 -0600

    Initial Commit

---
+++ Vidly/Models/Customers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Razor;
+
+namespace Vidly.Models
+{
+    public class Customers
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+
+ 
+}
